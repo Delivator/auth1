c6ff5394414ed2e89a8a4c038566ddb69e0f3029
Delivator <info@delivator.me>
2021-12-12 15:27:46 +0100

    vue add vuetify

--- src/main.js
+++ src/main.js
@@ -3,11 +3,15 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
